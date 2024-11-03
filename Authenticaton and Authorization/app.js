614edec35508a8a4da96a5c6865a7cad0cb41ee2
wjxnl <079bei008.ioepc.edu.np>
2024-11-03 20:31:35 +0545

    initial commit

---
+++ Authenticaton and Authorization/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const userModel = require("./models/usermodel");
+const cookieParser = require ("cookie-parser");
+const path = require("path");
+
+app.set ("view engine" ,  ".ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use (express.static(path.join (__dirname , "public")));
+app.use(cookieParser());
+
+
+app.get('/', (req , res) => {
+  res.render("index");
+});
+app.get('/login', (req , res) => {
+    res.render("login");
+});
+app.get('/logout', (req , res) => {
+    res.cookie("token" , "");
+    res.redirect("/");
+});
+
+app.post('/create', (req , res) => {
+  let {username, email, password, age} = req.body;
+
+  bcrypt.genSalt(10,  (err , salt)=>{
+    bcrypt.hash(password, salt , async (err, hash) =>{
+        
+        const UserCreated = await userModel.create({
+          username,
+          email,
+          password: hash,
+          age
+        });
+        let token = jwt.sign({email}, "secret");
+        res.cookie("token" , token);
+    });
+    res.redirect("/login");
+  });
+
+});
+app.post('/login',async (req , res) => {
+    let {email, password} = req.body;
+    let user = await userModel.findOne({email});
+    if(!user){
+       
+        res.send("Something went wrong");
+    }
+    else{
+        bcrypt.compare(password , user.password, (err, result) =>{
+            if(result){
+                let token = jwt.sign({email: user.email} , secret);
+                res.cookie("token" , token);
+                res.send("Your are loged In ");
+            }
+            else{
+                res.send("Something went wrong")
+            }
+        });
+
+    }
+
+  
+});
+app.listen("3000");
